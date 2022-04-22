21610325a97fa0c6bfce4280991b8ee1e6a3b728
crowe828 <69489498+crowe828@users.noreply.github.com>
2022-04-22 08:55:06 -0400

    working on about section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import ComputerMan from "../../img/computerMan.png";
 import "./about.css";
 
 const About = () => {
-  return <h1>About Section</h1>;
+  return (
+    <div className="row row-about">
+      <div className="column column-about">
+        <h1 className="text-about-1">i like making apps that look pretty.</h1>
+        <h2 className="text-about-2">
+          this is my prettiest app. <FontAwesomeIcon icon={faArrowRight} />
+        </h2>
+      </div>
+      <div className="column">
+        <img src={ComputerMan} alt="Man with Computer" />
+      </div>
+    </div>
+  );
 };
 
 export default About;
