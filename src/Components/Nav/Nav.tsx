c8c533135ea1c376d798665a59cab42c6ee3d494
crowe828 <69489498+crowe828@users.noreply.github.com>
2022-05-06 08:52:40 -0400

    navbar now responsive on all screen sizes

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -9,10 +9,12 @@ const Nav = () => {
     <div className="nav">
       <div className="row">
         <div>
+          {/* <div className="avataaar-div"> */}
           <img src={Avataaar} alt="Avataaar" className="avataaar" />
-        </div>
-        <div>
-          <h1 className="name">Christian (Rowe).</h1>
+          {/* </div>
+        <div className="name-div"> */}
+          <span className="name">Christian (Rowe).</span>
+          {/* <h1 className="name">Christian (Rowe).</h1> */}
         </div>
       </div>
       <HamburgerMenu />
