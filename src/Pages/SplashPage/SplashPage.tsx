55ecc9bba983d868ddcd477d6f53cb7cbeeec687
crowe828 <69489498+crowe828@users.noreply.github.com>
2023-08-07 20:24:12 -0400

    working on creating new splash page

--- src/Pages/SplashPage/SplashPage.tsx
+++ src/Pages/SplashPage/SplashPage.tsx
@@ -1,30 +1,14 @@
 import { Link } from "react-router-dom";
-import "./SplashPage.css";
+import "./splashPage.css";
 
 const SplashPage = () => {
-  const td: any = { "--td": "1s" };
-  const bc: any = { "--bc": "#ee6055", "--d": "0.1s" };
-
   return (
     <div className="splash-page">
-      <h1 className="block-effect" style={td}>
-        <div className="splash-text">
-          <div className="block-reveal" style={bc}>
-            (Christian) Rowe.
-          </div>
-          <div className="block-reveal" style={bc}>
-            Welcome.
-          </div>
-          <Link to="/main">
-            <button
-              type="button"
-              className="btn btn-lg btn-outline-success splash-btn"
-            >
-              Enter.
-            </button>
-          </Link>
-        </div>
-      </h1>
+      <h1 id="main-header">Christian (Rowe).</h1>
+      <h2 id="sub-header">hello.</h2>
+      <Link to={"/main"} id="main-link">
+        <button id="main-button">continue</button>
+      </Link>
     </div>
   );
 };
