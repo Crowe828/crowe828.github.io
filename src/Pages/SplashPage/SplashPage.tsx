7d280dfb0d291969ec10f3937dc257e9e56d3926
crowe828 <69489498+crowe828@users.noreply.github.com>
2023-08-09 21:46:48 -0400

    Added gif for splash page

--- src/Pages/SplashPage/SplashPage.tsx
+++ src/Pages/SplashPage/SplashPage.tsx
@@ -3,12 +3,14 @@ import "./splashPage.css";
 
 const SplashPage = () => {
   return (
-    <div className="splash-page">
-      <h1 id="main-header">Christian (Rowe).</h1>
-      <h2 id="sub-header">hello.</h2>
-      <Link to={"/main"} id="main-link">
-        <button id="main-button">continue</button>
-      </Link>
+    <div id="pipes-gif">
+      <div className="splash-page">
+        <h1 id="main-header">Christian (Rowe).</h1>
+        <h2 id="sub-header">hello.</h2>
+        <Link to={"/main"} id="main-link">
+          <button id="main-button">continue</button>
+        </Link>
+      </div>
     </div>
   );
 };
