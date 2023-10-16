f6a333866df208c799d613b3236d08f9cb515923
crowe828 <69489498+crowe828@users.noreply.github.com>
2023-10-16 08:36:05 -0400

    Updated info in each section

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -5,10 +5,12 @@ const About = () => {
   return (
     <div className="row">
       <div className="column column-about">
-        <h1 className="text-about">i like making apps that look pretty.</h1>
+        <h1 className="text-about">
+          I like making apps that look pretty. I also like coffee... a lot.
+        </h1>
       </div>
       <div className="column">
-        <img src={ComputerMan} alt="Man with Computer" />
+        <img src={ComputerMan} alt="Man with Computer" id="man-with-computer" />
       </div>
     </div>
   );
