7e7410d6e39371f9940c646d305cd9d7b98316dd
crowe828 <69489498+crowe828@users.noreply.github.com>
2023-10-18 08:55:42 -0400

    adding projects for portfolio

--- src/Components/Work/Work.tsx
+++ src/Components/Work/Work.tsx
@@ -1,9 +1,26 @@
+import SpotifyImg from "../../img/spotify-player.png";
 import "./work.css";
 
 const Work = () => {
   return (
     <div className="row row-work">
       <div className="column column-work">
+        <div className="column column-personal-projects">
+          <h1 className="personal-projects-title">Personal Projects</h1>
+          <a href="https://spotify-mini.web.app">
+            <h3 className="spotify-mini-title">Spotify Mini</h3>
+          </a>
+          <a href="https://spotify-mini.web.app">
+            <img
+              className="spotify-mini-img"
+              src={SpotifyImg}
+              alt="screenshot of Spotify Mini player"
+            />
+          </a>
+          <a href="https://github.com/Crowe828/spotify-mini">
+            <h5 className="github-link">GitHub Repo</h5>
+          </a>
+        </div>
         <h1 className="text-work">
           When I'm not working with clients to solve their development needs,
           I'm either outside running or inside reading. Currently working on an
