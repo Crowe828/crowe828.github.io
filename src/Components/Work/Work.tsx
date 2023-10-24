031d72644506dcaf9715535ad71596d182baf69f
crowe828 <69489498+crowe828@users.noreply.github.com>
2023-10-24 08:57:28 -0400

    Updated portfolio projects

--- src/Components/Work/Work.tsx
+++ src/Components/Work/Work.tsx
@@ -1,4 +1,5 @@
 import SpotifyImg from "../../img/spotify-player.png";
+import Inkmap from "../../img/inkmap.png";
 import "./work.css";
 
 const Work = () => {
@@ -7,17 +8,56 @@ const Work = () => {
       <div className="column column-work">
         <div className="column column-personal-projects">
           <h1 className="personal-projects-title">Personal Projects</h1>
-          <a href="https://spotify-mini.web.app">
+          <a
+            href="https://spotify-mini.web.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <h3 className="spotify-mini-title">Spotify Mini</h3>
           </a>
-          <a href="https://spotify-mini.web.app">
+          <a
+            href="https://spotify-mini.web.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img
-              className="spotify-mini-img"
+              className="portfolio-img"
               src={SpotifyImg}
               alt="screenshot of Spotify Mini player"
             />
           </a>
-          <a href="https://github.com/Crowe828/spotify-mini">
+          <a
+            href="https://github.com/Crowe828/spotify-mini"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h5 className="github-link">GitHub Repo</h5>
+          </a>
+        </div>
+        <div className="column column-personal-projects">
+          <a
+            href="https://christianrowe.dev/inkmap/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h3 className="portfolio-title">InkMap</h3>
+          </a>
+          <a
+            href="https://christianrowe.dev/inkmap/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="portfolio-img"
+              src={Inkmap}
+              alt="screenshot of InkMap's splash page"
+            />
+          </a>
+          <a
+            href="https://github.com/Crowe828/inkmap"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <h5 className="github-link">GitHub Repo</h5>
           </a>
         </div>
